81261bb01cb26e460a22d5b40d436138fdd38a1f
Monte Hazboun <P1127283@CORP.AD.PUBLIX.COM>
2017-08-02 10:18:42 -0400

    First commit

---
+++ DSCResources/pGAC/pGAC.psm1
@@ -0,0 +1,102 @@
+﻿Function Get-TargetResource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$AssemblyDLL, 
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+    $CurrentAssemblies = [System.AppDomain]::CurrentDomain.GetAssemblies()
+
+    $thisassembly = $CurrentAssemblies | where {$_.location -eq $AssemblyDLL}
+
+    $presence = if ($thisassembly.GlobalAssemblyCache) {"Present"} else {"Absent"}
+
+    return @{
+        AssemblyDLL = $AssemblyDLL
+        Ensure = $presence
+    }
+}
+
+Function Test-TargetResource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$AssemblyDLL, 
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+    $CurrentAssemblies = [System.AppDomain]::CurrentDomain.GetAssemblies()
+
+    $thisassembly = $CurrentAssemblies | where {$_.location -eq $AssemblyDLL}
+    
+    
+switch ($ensure) {
+    "Present" {
+        return $ThisAssembly.GlobalAssemblyCache
+        }
+    "Absent" {
+        if ($thisassembly.GlobalAssemblyCache) {
+            return $false
+        } else {
+            return $true
+        }
+    }
+}
+}
+
+Function Set-TargetResource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$AssemblyDLL, 
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+   if (!(Test-Path $assemblyDLL -type Leaf) ) {
+        throw "The assembly '$assemblyDLL' does not exist."
+   }
+
+   if (!(Test-IsStronglySigned $AssemblyDLL)) {
+        throw "$AssemblyDLL is not strongly signed"
+   }
+
+   if (([AppDomain]::CurrentDomain.GetAssemblies() |? { $_.FullName -eq "System.EnterpriseServices, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" }) -eq $null ) {
+        [System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a") | Out-Null
+   }
+   $publish = New-Object System.EnterpriseServices.Internal.Publish
+
+   switch ($Ensure) {
+        "Present" {     
+            $publish.GacInstall($assemblyDLL)
+        }
+        "Absent" {
+            $publish.GacRemove($assemblyDLL)     
+        }
+   }
+}
+
+
+
+
+Function Test-IsStronglySigned {
+    param (
+        $assembly
+    )
+     if ( [System.Reflection.Assembly]::LoadFile($assembly).GetName().GetPublicKey().Length -eq 0 ) {
+        return $false
+     } else {
+        return $true
+     }
+}
+
+Export-ModuleMember *-TargetResource
